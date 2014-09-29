7c6ccd60149cfd7001db798089ce3d7f0143831a
dragmove <dragmove@naver.com>
2014-09-30 01:43:39 +0900

    update yuidoc

---
+++ out/api.js
@@ -0,0 +1,33 @@
+YUI.add("yuidoc-meta", function(Y) {
+   Y.YUIDoc = { meta: {
+    "classes": [
+        "ArrayUtil",
+        "BrowserUtil",
+        "CanvasUtil",
+        "CheckUtil",
+        "ClipCircle",
+        "ControlUtil",
+        "ControlUtil.SimpleTimer",
+        "CookieUtil",
+        "DateData",
+        "Debug",
+        "EffectUtil",
+        "EffectUtil.EntranceExpansion",
+        "EffectUtil.TypeWriter",
+        "FormUtil",
+        "ImageLoader",
+        "InitUtil",
+        "InteractionUtil",
+        "LanguageData",
+        "MathUtil",
+        "MovieClip",
+        "NodeUtil",
+        "RegExpData",
+        "SimpleVideo",
+        "StringUtil",
+        "XmlUtil"
+    ],
+    "modules": [],
+    "allModules": []
+} };
+});
